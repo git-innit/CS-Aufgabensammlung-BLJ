3cead8a0234130b3dc7e781407c24cb946f327ea
git-innit <187617015+git-innit@users.noreply.github.com>
2024-11-08 11:04:40 +0100

    Add Aufgabe 19

---
+++ Aufgabe-20/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace Aufgabe_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var client = new RestClient("https://witzapi.de");
+                var request = new RestRequest("api/joke/", Method.Get);
+                var response = client.Get(request);
+
+                JArray array = JArray.Parse(response.Content);
+                string text = array[0]["text"].ToString();
+
+                Console.WriteLine(text + "\n");
+                Console.Write("Nächsten Witz holen? j/n  ");
+                if (Console.ReadLine().ToLower() == "j")
+                {
+                    Console.Write("\n");
+                    continue;
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
