ee2b4575bbc9483b82cfa4fda3cf8590f1cdf1d7
git-innit <187617015+git-innit@users.noreply.github.com>
2024-11-08 09:02:20 +0100

    Add Aufgabe 19

---
+++ Aufgabe-19/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Aufgabe_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    int output = 0;
+
+                    Console.WriteLine("Make your calculation (or press Q to quit):");
+                    string eingabe = Console.ReadLine();
+                    if (eingabe.ToLower() == "q")
+                    {
+                        Environment.Exit(0);
+                    }
+                    eingabe = eingabe.Trim();
+
+                    if (eingabe.Contains("+"))
+                    {
+                        output = Rechner(eingabe, '+');
+                    }
+                    else if (eingabe.Contains("-"))
+                    {
+                        output = Rechner(eingabe, '-');
+                    }
+                    else if (eingabe.Contains("*"))
+                    {
+                        output = Rechner(eingabe, '*');
+                    }
+                    else if (eingabe.Contains("/"))
+                    {
+                        output = Rechner(eingabe, '/');
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+
+                    Console.WriteLine(output);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ungültige Eingabe.\n");
+                    continue;
+                }
+            }
+        }
+        private static int Rechner(string eingabe,char aktion)
+        {
+            string[] eingabe_zahlen_str = eingabe.Split(aktion);
+            int[] zahlen = new int[eingabe_zahlen_str.Length];
+            int rueckgabewert = 0;
+            for (int i = 0; i < eingabe_zahlen_str.Length; i++)
+            {
+                zahlen[i] = Convert.ToInt32(eingabe_zahlen_str[i]);
+            }
+            switch (aktion)
+            {
+                case '+': rueckgabewert = zahlen[0] + zahlen[1]; break;
+                case '-': rueckgabewert = zahlen[0] - zahlen[1]; break;
+                case '*': rueckgabewert = zahlen[0] * zahlen[1]; break;
+                case '/': rueckgabewert = zahlen[0] / zahlen[1]; break;
+                default: throw new Exception();
+            }
+            return rueckgabewert;
+        }
+    }
+}
